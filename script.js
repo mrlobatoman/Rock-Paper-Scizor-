3d5a0e9bbad93252de881d0c5eb2c80c7053d3e2
Michael Lobato <mike.lobato.wft@gmail.com>
2023-01-16 12:09:26 -0800

    Add: functions that check user and computer choices and displays the correlating image

--- script.js
+++ script.js
@@ -9,13 +9,28 @@ var changeText = document.querySelector('.computer-message')
 var playerOptions = document.querySelector('.games-container')
 var fullTeam1 = document.querySelector('.full-team-1')
 var fullTeam2 = document.querySelector('.full-team-2')
-var displayOptions = document.querySelector('.display-options')
-var poisonOption = document.querySelector('.poison')
-var rockOption = document.querySelector('.rock')
+var poisonOption = document.querySelector('.poison-emoji')
+var rockOption = document.querySelector('.rock-emoji')
+var changeGameButton = document.querySelector('.change-game')
+var battleIcons = document.querySelector('.battle-icons')
+var userChoice = document.querySelector('.user-chosen-icon')
+var computerChoice = document.querySelector('.computer-chosen-icon')
+var displayChoices = document.querySelector('.icon-selection')
 
 
 
+changeGameButton.addEventListener('click', changeGame)
 
+
+function changeGame(){
+    if(currentGame.gameType === 'easy'){
+        currentGame.gameType ='hard'
+    } else {
+        currentGame.gameType = 'easy'
+    }
+    hide(displayChoices)
+    show(battleIcons)
+}
 
 easyMode.addEventListener('click', function() {
     easyGame()
@@ -24,7 +39,14 @@ easyMode.addEventListener('click', function() {
 
 difficultMode.addEventListener('click', function() {
     hardGame()
-    currentGame.changeGameVersion(hardMode.closest('#hard').id)
+    currentGame.changeGameVersion(difficultMode.closest('#hard').id)
+})
+
+
+battleIcons.addEventListener('click', function(){
+    var targetedId = event.target.id
+    currentGame.updateSelections(targetedId)
+    displayChosen()
 })
 
 function show(element){
@@ -42,28 +64,64 @@ function prompt(){
 
 
 function easyGame(){
+    console.log('hello')
     hide(easyMode)
     hide(difficultMode)
     hide(chooseYourGame)
     show(userPokemonTeam)
     show(trPokemonTeam)
-    show(displayOptions)
+    show(battleIcons)
     prompt()
 }
 
 function hardGame(){
+    console.log('hello')
     hide(easyMode)
     hide(difficultMode)
     hide(chooseYourGame)
     show(userPokemonTeam)
-    show(fullTeam1)
-    show(fullTeam2)
     show(trPokemonTeam)
-    show(displayOptions)
+    show(battleIcons)
     show(poisonOption)
     show(rockOption)
     prompt()
-   
+}
+
+function checkUserChoice(){
+    if (currentGame.user.choice === 'fire') {
+        userChoice.src = "./images/trainer/charmander front.png"
+    } else if (currentGame.user.choice === 'water') {
+        userChoice.src = "./images/trainer/squirtle front.png"
+    } else if (currentGame.user.choice === 'grass') {
+        userChoice.src = "./images/trainer/bulbasaur front.png"
+    } else if (currentGame.user.choice === 'poison') {
+        userChoice.src = "./images/trainer/grimer.png"
+    } else if (currentGame.user.choice === 'rock') {
+        userChoice.src = "./images/trainer/geodude.png"
+    }
+}
+
+function checkComputerChoice(){
+    if (currentGame.computer.choice === 'fire') {
+        computerChoice.src = "./images/team rocket/growlithe.png"
+    } else if (currentGame.computer.choice === 'water') {
+        computerChoice.src = "./images/team rocket/horsea.png"
+    } else if (currentGame.computer.choice === 'grass') {
+        computerChoice.src = "./images/team rocket/bellsprout.png"
+    } else if (currentGame.computer.choice === 'poison') {
+        computerChoice.src = "./images/team rocket/ekans.png"
+    } else if (currentGame.computer.choice === 'rock') {
+        computerChoice.src = "./images/team rocket/rhyhorn.png"
+    }
 }
 
 
+ 
+
+
+function displayChosen(){
+    hide(playerOptions)
+    checkUserChoice()
+    checkComputerChoice()
+    show(displayChoices)
+}
