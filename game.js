8eabb0437a0a1a343442e466524ab9876b979f73
Michael Lobato <mike.lobato.wft@gmail.com>
2023-01-17 13:12:00 -0800

    Refactor: indentation

--- game.js
+++ game.js
@@ -1,51 +1,51 @@
 class Game{
-    constructor(){
-        this.gameType = 'easy';
-        this.user = new Trainer ('user')
-        this.computer = new Trainer('computer')
-        this.easyChoices = ['fire', 'grass', 'water']
-        this.difficultChoices = ['fire', 'grass', 'water', 'rock', 'poison']
-        this.winner = null;
+  constructor(){
+    this.gameType = 'easy';
+    this.user = new Trainer ('user')
+    this.computer = new Trainer('computer')
+    this.easyChoices = ['fire', 'grass', 'water']
+    this.difficultChoices = ['fire', 'grass', 'water', 'rock', 'poison']
+    this.winner = null;
+  }
+  changeGameVersion(gameId) {
+    if(gameId === 'easy'){
+        this.gameType = 'easy'
+    } else if (gameId === 'hard'){
+        this.gameType = 'hard'
     }
-    changeGameVersion(gameId) {
-        if(gameId === 'easy'){
-            this.gameType = 'easy'
-        } else if (gameId === 'hard'){
-            this.gameType = 'hard'
-        }
+  }
+  updateSelections(targetedId){
+    if(this.gameType === 'easy'){
+      this.user.easyTurn(targetedId)
+      this.computer.easyTurn()
     }
-    updateSelections(targetedId) {
-        if(this.gameType === 'easy') {
-            this.user.easyTurn(targetedId)
-            this.computer.easyTurn()
-        }
-        if(this.gameType === 'hard') {
-            this.user.hardTurn(targetedId)
-            this.computer.hardTurn()
-        }
-        this.winner = this.determineWinner()
+    if(this.gameType === 'hard'){
+      this.user.hardTurn(targetedId)
+      this.computer.hardTurn()
     }
-    determineWinner() {
-        if(this.user.choice === this.computer.choice){
-            this.winner = null
-        } else if(this.user.choice === 'fire' && (this.computer.choice === 'grass' || this.computer.choice === 'poison')) {
-            this.user.winRound()
-            return 'trainer'
-        } else if(this.user.choice === 'water' && (this.computer.choice === 'fire' || this.computer.choice === 'rock')) {
-            this.user.winRound()
-            return 'trainer'
-        }  else if(this.user.choice === 'grass' && (this.computer.choice === 'water' || this.computer.choice === 'rock')) {
-            this.user.winRound()
-            return 'trainer'
-        }  else if(this.user.choice === 'poison' && (this.computer.choice === 'grass' || this.computer.choice === 'water')) {
-            this.user.winRound()
-            return 'trainer'
-        }  else if(this.user.choice === 'rock' && (this.computer.choice === 'fire' || this.computer.choice === 'poison')) {
-            this.user.winRound()
-            return 'trainer'
-        } else {
-            this.computer.winRound()
-            return 'computer'
-        }
-    }
+      this.winner = this.determineWinner()
+  }
+  determineWinner() {
+    if(this.user.choice === this.computer.choice){
+      this.winner = null
+    } else if(this.user.choice === 'fire' && (this.computer.choice === 'grass' || this.computer.choice === 'poison')){
+      this.user.winRound()
+      return 'trainer'
+    } else if(this.user.choice === 'water' && (this.computer.choice === 'fire' || this.computer.choice === 'rock')){
+      this.user.winRound()
+      return 'trainer'
+    } else if(this.user.choice === 'grass' && (this.computer.choice === 'water' || this.computer.choice === 'rock')){
+      this.user.winRound()
+      return 'trainer'
+    } else if(this.user.choice === 'poison' && (this.computer.choice === 'grass' || this.computer.choice === 'water')){
+      this.user.winRound()
+      return 'trainer'
+    } else if(this.user.choice === 'rock' && (this.computer.choice === 'fire' || this.computer.choice === 'poison')){
+      this.user.winRound()
+      return 'trainer'
+    } else {
+      this.computer.winRound()
+      return 'computer'
+    }    
+  }
 }
