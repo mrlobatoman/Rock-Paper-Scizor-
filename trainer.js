476297929b2ea174a046f5d66322288cebfe4f21
Michael Lobato <mike.lobato.wft@gmail.com>
2023-01-17 13:26:14 -0800

    Refactor: indentation

--- trainer.js
+++ trainer.js
@@ -1,19 +1,19 @@
 class Trainer{
-    constructor(playerName){
-        this.name = playerName
-        this.choice = null
-        this.score = 0
-    }
-    easyTurn(targetedId){
-        this.choice = targetedId || this.getRandomElement(currentGame.easyChoices)
-    }
-    hardTurn(targetedId){
-        this.choice = targetedId || this.getRandomElement(currentGame.difficultChoices)
-    }
-    getRandomElement(array) {
-        return array[Math.floor(Math.random()*array.length)];
-      }
-    winRound(){
-        this.score++
-    }
+  constructor(playerName){
+    this.name = playerName
+    this.choice = null
+    this.score = 0
+  }
+  easyTurn(targetedId){
+    this.choice = targetedId || this.getRandomElement(currentGame.easyChoices)
+  }
+  hardTurn(targetedId){
+    this.choice = targetedId || this.getRandomElement(currentGame.difficultChoices)
+  }
+  getRandomElement(array) {
+    return array[Math.floor(Math.random()*array.length)];
+  }
+  winRound(){
+    this.score++
+  }
 }
